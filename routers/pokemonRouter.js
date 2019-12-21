347295c4a2991d1cb25f5aed272e65d050cd9215
weichichou <weichi.cc@gmail.com>
2019-12-21 17:53:55 +0100

    Add test for water to grass is half damage

--- routers/pokemonRouter.js
+++ routers/pokemonRouter.js
@@ -1,7 +1,12 @@
 const typeMatchup = async(enemy, mine, superagent) => {
     const res = await superagent.get(`https://pokeapi.co/api/v2/type/${mine}`)
-    const found = res.body.damage_relations.double_damage_to.find(item => item.name === enemy)    
-    if (found){return 'double_damage_to'}
+    const { damage_relations } = res.body
+    for (const property in damage_relations){
+        const found = damage_relations[property].find(item => item.name === enemy)
+        if(found){
+            return property
+        }
+    }
 }
 
 module.exports = { typeMatchup }
--- tests/pokemonRouter.test.js
+++ tests/pokemonRouter.test.js
@@ -1,22 +1,32 @@
 const { typeMatchup } = require('../routers/pokemonRouter')
 
 describe('Damage Relation', () => {
-    test('Water versus fire is double damage', async() => {
+    test('Water to fire is double damage', async() => {
         const enemy = 'fire';
         const mine = 'water';
         const superagent = {
             "get": jest.fn().mockResolvedValue({"body": {
                 "damage_relations":{
-                    "double_damage_to":[
-                        {
-                            "name":"fire",
-                            "url":"https://pokeapi.co/api/v2/type/10/"
-                        }
-                    ]
+                    "double_damage_to":[{ "name": "fire" }]
                 }
             }})
         }
         const damageRelation = await typeMatchup(enemy, mine, superagent);
         expect(damageRelation).toEqual('double_damage_to')
     })
+
+    test('Water to grass is half damage', async() => {
+        const enemy = 'grass';
+        const mine = 'water';
+        const superagent = {
+            "get": jest.fn().mockResolvedValue({"body": {
+                "damage_relations":{
+                    "half_damage_to":[{ "name": "grass" }]
+                }
+            }})
+        }
+        const damageRelation = await typeMatchup(enemy, mine, superagent);
+        expect(damageRelation).toEqual('half_damage_to')
+    })
+
 })
