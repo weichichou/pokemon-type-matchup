8517a7eb4e577ffbebf79025feae0a4895d5669a
weichichou <weichi.cc@gmail.com>
2019-12-24 15:58:59 +0100

    Practice on data and loop

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="small-container">
+      <h1>Employees</h1>
+
+      <employee-table :employees='employees' />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+      EmployeeTable
+    },
+    data() {
+      return {
+        employees: [
+          {
+            id: 1,
+            name: 'Rich',
+            email: 'rich@rich.com'
+          },
+          {
+            id: 2,
+            name: 'Bert',
+            email: 'bert@bert.com'
+          },
+          {
+            id: 3,
+            name: 'Di',
+            email: 'di@di.com'
+          }
+        ]
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    button {
+      background: #009435;
+      border: 1px solid #009435;
+    }
+
+    .small-container {
+      max-width: 6980px;
+    }
 </style>
