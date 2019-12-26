a05c4bae5ea2f2e3cb214ab4f93ac320b331e3b2
weichichou <weichi.cc@gmail.com>
2019-12-26 11:16:58 +0100

    User can delete an item

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,8 @@
     <div id="app" class="small-container">
       <h1>Employees</h1>
       <employee-form @add:employee="addEmployee" />
-      <employee-table :employees='employees' />
+      <employee-table :employees='employees'
+      @delete:employee="deleteEmployee" />
     </div>
 </template>
 
@@ -47,6 +48,11 @@ export default {
             const newEmployee = { ...employee, id };
             
             this.employees = [...this.employees, newEmployee]
+        },
+        deleteEmployee(id){
+          this.employees = this.employees.filter(
+            employee => employee.id !== id
+          )
         }  
     }
 }
