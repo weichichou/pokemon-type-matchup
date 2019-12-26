19e891cfa6f7abbdde5375848967cb94fa43f638
weichichou <weichi.cc@gmail.com>
2019-12-26 17:00:00 +0100

    User can edit an item and save

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,70 +1,80 @@
+<!-- : is shorthand for v-bind -->
+
 <template>
     <div id="app" class="small-container">
-      <h1>Employees</h1>
-      <employee-form @add:employee="addEmployee" />
-      <employee-table :employees='employees'
-      @delete:employee="deleteEmployee" />
+        <h1>Employees</h1>
+        <employee-form @add:employee="addEmployee" />
+        <employee-table
+            :employees="employees"
+            @delete:employee="deleteEmployee"
+            @edit:employee="editEmployee"
+        />
     </div>
 </template>
 
 <script>
-import EmployeeForm from './components/EmployeeForm.vue'
-import EmployeeTable from './components/EmployeeTable.vue'
+import EmployeeForm from "./components/EmployeeForm.vue";
+import EmployeeTable from "./components/EmployeeTable.vue";
 
 export default {
-    name: 'app',
+    name: "app",
     components: {
-      EmployeeTable,
-      EmployeeForm,
+        EmployeeTable,
+        EmployeeForm
     },
     data() {
-      return {
-        employees: [
-          {
-            id: 1,
-            name: 'Rich',
-            email: 'rich@rich.com'
-          },
-          {
-            id: 2,
-            name: 'Bert',
-            email: 'bert@bert.com'
-          },
-          {
-            id: 3,
-            name: 'Di',
-            email: 'di@di.com'
-          }
-        ]
-      }
+        return {
+            employees: [
+                {
+                    id: 1,
+                    name: "Rich",
+                    email: "rich@rich.com"
+                },
+                {
+                    id: 2,
+                    name: "Bert",
+                    email: "bert@bert.com"
+                },
+                {
+                    id: 3,
+                    name: "Di",
+                    email: "di@di.com"
+                }
+            ]
+        };
     },
     methods: {
-        addEmployee(employee){
-            const lastId = 
+        addEmployee(employee) {
+            const lastId =
                 this.employees.length > 0
                     ? this.employees[this.employees.length - 1].id
                     : 0;
             const id = lastId + 1;
             const newEmployee = { ...employee, id };
-            
-            this.employees = [...this.employees, newEmployee]
+
+            this.employees = [...this.employees, newEmployee];
         },
-        deleteEmployee(id){
-          this.employees = this.employees.filter(
-            employee => employee.id !== id
-          )
-        }  
+        deleteEmployee(id) {
+            this.employees = this.employees.filter(
+                employee => employee.id !== id
+            );
+        },
+        editEmployee(id, updatedEmployee) {
+            this.employees = this.employees.map(employee =>
+                employee.id === id ? updatedEmployee : employee
+            );
+        }
     }
-}
+};
 </script>
 
 <style>
-    button {
-      background: #009435;
-      border: 1px solid #009435;
-    }
+button {
+    background: #009435;
+    border: 1px solid #009435;
+}
 
-    .small-container {
-      max-width: 6980px;
-    }
+.small-container {
+    max-width: 6980px;
+}
 </style>
--- client/src/components/EmployeeTable.vue
+++ client/src/components/EmployeeTable.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id='employee-table'>
-        <p v-if='employees.length < 1' class='empty-table'>
+    <div id="employee-table">
+        <p v-if="employees.length < 1" class="empty-table">
             No employees
         </p>
         <table v-else>
@@ -12,33 +12,67 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for='employee in employees' :key="employee.id">
-                    <td>{{ employee.name }}</td>
-                    <td>{{ employee.email }}</td>
-                    <td>
-                        <button>Edit</button>
-                        <button 
-                        @click="$emit('delete:employee', employee.id)">
+                <tr v-for="employee in employees" :key="employee.id">
+                    <td v-if="editing === employee.id">
+                        <input type="text" v-model="employee.name" />
+                    </td>
+                    <td v-else>{{ employee.name }}</td>
+
+                    <td v-if="editing === employee.id">
+                        <input type="text" v-model="employee.email" />
+                    </td>
+                    <td v-else>{{ employee.email }}</td>
+
+                    <td v-if="editing === employee.id">
+                        <button @click="editEmployee(employee)">Save</button>
+                        <button class="muted-button" @click="editing = null">
+                            Cancel
+                        </button>
+                    </td>
+                    <td v-else>
+                        <button @click="editMode(employee.id)">
+                            Edit
+                        </button>
+                        <button @click="$emit('delete:employee', employee.id)">
                             Delete
                         </button>
                     </td>
                 </tr>
-            </tbody> 
+            </tbody>
         </table>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'employee-table',
-        props: {
-            employees: Array,
+export default {
+    name: "employee-table",
+    data() {
+        return {
+            editing: null
+        };
+    },
+    methods: {
+        editMode(id) {
+            this.editing = id;
+        },
+        editEmployee(employee) {
+            if (employee.name === "" || employee.email === "") return;
+            this.$emit("edit:employee", employee.id, employee);
+            this.editing = null;
         }
+    },
+    props: {
+        employees: Array
     }
+};
 </script>
 
 <style scoped>
-    button {
-        margin: 0 0.5rem 0 0;
-    }
+button {
+    margin: 0 0.5rem 0 0;
+}
+
+.muted-button {
+    background-color: grey;
+}
 </style>
