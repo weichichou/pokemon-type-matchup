affe1cea6f62621c0fa932edb20d22716a88125e
weichichou <weichi.cc@gmail.com>
2019-12-27 12:22:57 +0100

    Refactor makeSentence function

--- client/src/components/MatchupForm.vue
+++ client/src/components/MatchupForm.vue
@@ -7,7 +7,11 @@
             <input type="text" v-model="matchup.enemy" />
             <button>Show Results</button>
         </form>
-        <div v-if="results !== ''">{{ results }}</div>
+        <div v-if="results.length > 0">
+            <p v-for="result in results" :key="result">
+                {{ result }}
+            </p>
+        </div>
     </div>
 </template>
 
@@ -23,22 +27,33 @@ export default {
                 mine: "",
                 enemy: ""
             },
-            results: ""
+            results: []
         };
     },
     methods: {
         async handleSubmit() {
-            // console.log("clicked?");
             try {
                 const response = await fetch(
                     `http://localhost:3000/matchup?mine=${this.matchup.mine}&enemy=${this.matchup.enemy}`
                 );
-                console.log("response?", response);
-                this.results = await response.text();
-                console.log("result?", this.results);
+                const data = await response.json();
+                this.results = this.makeSentence(data);
             } catch (error) {
                 console.error(error);
             }
+        },
+        makeSentence(array) {
+            const convert = {
+                double_damage_to: "You will make 2x damage to the enemy.",
+                double_damage_from:
+                    "You will receive 2x damage from the enemy.",
+                half_damage_to: "You will make 0.5x damage to the enemy.",
+                half_damage_from:
+                    "You will receive 0.5x damage from the enemy.",
+                no_damage_to: "You will make no damage to the enemy.",
+                no_damage_from: "You will receive no damage from the enemy."
+            };
+            return array.map(item => convert[item]);
         }
     }
 };
