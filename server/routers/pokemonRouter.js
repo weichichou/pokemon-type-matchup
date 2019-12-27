7656253940db7e1cdce48f1159bfbf34151690d5
weichichou <weichi.cc@gmail.com>
2019-12-27 11:13:40 +0100

    Adjust test for damage relations

--- server/routers/pokemonRouter.js
+++ server/routers/pokemonRouter.js
@@ -1,51 +1,53 @@
-const getType = async(input, httpClient) => {
-    let res 
-    try {
-        res = await httpClient.get(`https://pokeapi.co/api/v2/pokemon/${input}`)
-    }
-    catch(error) {
-        console.error(error)
-        throw error
-    }
-    const { types } = res.body
-    const result = types.map(item => item.type.name)
-    return result[0]
-}
+const getType = async (input, httpClient) => {
+  let res;
+  try {
+    res = await httpClient.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+  const { types } = res.body;
+  const result = types.map(item => item.type.name);
+  return result[0];
+};
 
-const typeMatchup = async(enemy, mine, httpClient) => {
-    let res
-    try {
-        res = await httpClient.get(`https://pokeapi.co/api/v2/type/${mine}`)
+const typeMatchup = async (enemy, mine, httpClient) => {
+  let res;
+  try {
+    res = await httpClient.get(`https://pokeapi.co/api/v2/type/${mine}`);
+  } catch (error) {
+    console.error(error);
+  }
+  const { damage_relations } = res.body;
+  let result = [];
+  for (const property in damage_relations) {
+    const found = damage_relations[property].find(item => item.name === enemy);
+    if (found) {
+      result.push(property);
     }
-    catch(error){
-        console.error(error)
-    }
-    const { damage_relations } = res.body
-    for (const property in damage_relations){
-        const found = damage_relations[property].find(item => item.name === enemy)
-        if(found){
-            return property
-        }
-    }
-    return 'normal effectiveness'
-}
+  }
+  if (result.length !== 0) {
+    return result;
+  } else {
+    return "normal effectiveness";
+  }
+};
 
-const superagent = require('superagent')
-const matchupHandler = async(req, res) => {
-    const { mine, enemy } = req.query
-    
-    try {
-        const myType = await getType(mine, superagent)
-        const enemyType = await getType(enemy, superagent)
-        const result = await typeMatchup(enemyType, myType, superagent)
-        res.status(200).send(result)
-    }
-    catch(error){
-        if(error.status === 404){
-            res.status(404).send('Please check your input')
-        }
-        res.status(500).send('Failed to call the pokeapi')
-    }
-}
+const superagent = require("superagent");
+const matchupHandler = async (req, res) => {
+  const { mine, enemy } = req.query;
 
-module.exports = { getType, typeMatchup, matchupHandler }
+  try {
+    const myType = await getType(mine, superagent);
+    const enemyType = await getType(enemy, superagent);
+    const result = await typeMatchup(enemyType, myType, superagent);
+    res.status(200).send(result);
+  } catch (error) {
+    if (error.status === 404) {
+      res.status(404).send("Please check your input");
+    }
+    res.status(500).send("Failed to call the pokeapi");
+  }
+};
+
+module.exports = { getType, typeMatchup, matchupHandler };
--- server/tests/pokemonRouter.test.js
+++ server/tests/pokemonRouter.test.js
@@ -1,87 +1,106 @@
-const { getType, typeMatchup, matchupHandler } = require('../routers/pokemonRouter')
+const {
+  getType,
+  typeMatchup,
+  matchupHandler
+} = require("../routers/pokemonRouter");
 
-describe('Get Type By Name', () => {
-    test('butterfree is flying-type', async() => {
-        const input = 'butterfree';
-        const superagent = {
-            "get": jest.fn().mockResolvedValue({"body": {
-                "types": [{
-                    "type": { "name": "flying" }
-                }]
-            }})
+describe("Get Type By Name", () => {
+  test("butterfree is flying-type", async () => {
+    const input = "butterfree";
+    const superagent = {
+      get: jest.fn().mockResolvedValue({
+        body: {
+          types: [
+            {
+              type: { name: "flying" }
+            }
+          ]
         }
-        const type = await getType(input, superagent);
-        expect(type).toEqual('flying')
-    })
+      })
+    };
+    const type = await getType(input, superagent);
+    expect(type).toEqual("flying");
+  });
 
-    test('charizard is flying and fire-type', async() => {
-        const input = 'charizard';
-        const superagent = {
-            "get": jest.fn().mockResolvedValue({"body": {
-                "types": [
-                    { "type": { "name": "flying" } }, 
-                    { "type": { "name": "fire" } }
-                ]
-            }})
+  test("charizard is flying and fire-type", async () => {
+    const input = "charizard";
+    const superagent = {
+      get: jest.fn().mockResolvedValue({
+        body: {
+          types: [{ type: { name: "flying" } }, { type: { name: "fire" } }]
         }
-        const type = await getType(input, superagent);
-        expect(type).toEqual('flying')
-    })
-})
+      })
+    };
+    const type = await getType(input, superagent);
+    expect(type).toEqual("flying");
+  });
+});
 
-describe('Damage Relation', () => {
-    test('Water to fire is double damage', async() => {
-        const enemy = 'fire';
-        const mine = 'water';
-        const superagent = {
-            "get": jest.fn().mockResolvedValue({"body": {
-                "damage_relations":{
-                    "double_damage_to":[{ "name": "fire" }]
-                }
-            }})
+describe("Damage Relation", () => {
+  test("Water to fire will make 2x damage and receive 0.5 damage", async () => {
+    const enemy = "fire";
+    const mine = "water";
+    const superagent = {
+      get: jest.fn().mockResolvedValue({
+        body: {
+          damage_relations: {
+            double_damage_to: [{ name: "fire" }],
+            half_damage_from: [{ name: "fire" }]
+          }
         }
-        const damageRelation = await typeMatchup(enemy, mine, superagent);
-        expect(damageRelation).toEqual('double_damage_to')
-    })
+      })
+    };
+    const damageRelation = await typeMatchup(enemy, mine, superagent);
+    expect(damageRelation).toEqual(["double_damage_to", "half_damage_from"]);
+  });
 
-    test('Water to grass is half damage', async() => {
-        const enemy = 'grass';
-        const mine = 'water';
-        const superagent = {
-            "get": jest.fn().mockResolvedValue({"body": {
-                "damage_relations":{
-                    "half_damage_to":[{ "name": "grass" }]
-                }
-            }})
+  test("Water to grass will make 0.5 damage and receive 2x damage", async () => {
+    const enemy = "grass";
+    const mine = "water";
+    const superagent = {
+      get: jest.fn().mockResolvedValue({
+        body: {
+          damage_relations: {
+            double_damage_from: [{ name: "grass" }],
+            half_damage_to: [{ name: "grass" }]
+          }
         }
-        const damageRelation = await typeMatchup(enemy, mine, superagent);
-        expect(damageRelation).toEqual('half_damage_to')
-    })
+      })
+    };
+    const damageRelation = await typeMatchup(enemy, mine, superagent);
+    expect(damageRelation).toEqual(["double_damage_from", "half_damage_to"]);
+  });
 
-    test('Water to poison is normal effectiveness', async() => {
-        const enemy = 'poison';
-        const mine = 'water';
-        const superagent = {
-            "get": jest.fn().mockResolvedValue({"body": {
-                "damage_relations":{ }
-            }})
+  test("Water to poison is normal effectiveness", async () => {
+    const enemy = "poison";
+    const mine = "water";
+    const superagent = {
+      get: jest.fn().mockResolvedValue({
+        body: {
+          damage_relations: {}
         }
-        const damageRelation = await typeMatchup(enemy, mine, superagent);
-        expect(damageRelation).toEqual('normal effectiveness')
-    })
-})
+      })
+    };
+    const damageRelation = await typeMatchup(enemy, mine, superagent);
+    expect(damageRelation).toEqual("normal effectiveness");
+  });
+});
 
-describe('Get Type By Name', () => {
-    test('butterfree is flying-type', async() => {
-        const input = 'butterfree';
-        const superagent = {
-            "get": jest.fn().mockResolvedValue({"body": {
-                "types": [{
-                    "type": { "name": "flying" }
-                }]
-            }})
+describe("Get Type By Name", () => {
+  test("butterfree is flying-type", async () => {
+    const input = "butterfree";
+    const superagent = {
+      get: jest.fn().mockResolvedValue({
+        body: {
+          types: [
+            {
+              type: { name: "flying" }
+            }
+          ]
         }
-        const type = await getType(input, superagent);
-        expect(type).toEqual('flying')
-    })
-})
+      })
+    };
+    const type = await getType(input, superagent);
+    expect(type).toEqual("flying");
+  });
+});
