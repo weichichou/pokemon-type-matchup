69656011726385838c01b14661b9ac553ae802b0
weichichou <weichi.cc@gmail.com>
2019-12-29 23:02:06 +0100

    Delete practice components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,3 @@
-<!-- : is shorthand for v-bind -->
-
 <template>
     <div id="app" class="small-container">
         <h1>Pokemon Matchup</h1>
@@ -7,31 +5,17 @@
             <type-form :side="mine" @set:poke="setMine" />
             <type-form :side="enemy" @set:poke="setEnemy" />
         </div>
-
         <matchup :mine="minePoke" :enemy="enemyPoke" />
-
-        <!-- <pokemon-table :pokemons="pokemons" /> -->
-
-        <!-- <employee-form @add:employee="addEmployee" />
-        <employee-table
-            :employees="employees"
-            @delete:employee="deleteEmployee"
-            @edit:employee="editEmployee"
-        /> -->
     </div>
 </template>
 
 <script>
 import TypeForm from "./components/TypeForm";
 import Matchup from "./components/Matchup";
-// import PokemonTable from "./components/PokemonTable";
-/* import EmployeeForm from "./components/EmployeeForm.vue";
-import EmployeeTable from "./components/EmployeeTable.vue"; */
 
 export default {
     name: "app",
     components: {
-        //PokemonTable
         TypeForm,
         Matchup
     },
@@ -51,37 +35,10 @@ export default {
             this.enemyPoke = pokemon;
         }
     }
-    /* methods: {
-        addEmployee(employee) {
-            const lastId =
-                this.employees.length > 0
-                    ? this.employees[this.employees.length - 1].id
-                    : 0;
-            const id = lastId + 1;
-            const newEmployee = { ...employee, id };
-
-            this.employees = [...this.employees, newEmployee];
-        },
-        deleteEmployee(id) {
-            this.employees = this.employees.filter(
-                employee => employee.id !== id
-            );
-        },
-        editEmployee(id, updatedEmployee) {
-            this.employees = this.employees.map(employee =>
-                employee.id === id ? updatedEmployee : employee
-            );
-        }
-    } */
 };
 </script>
 
 <style>
-/* button {
-    background: #009435;
-    border: 1px solid #009435;
-} */
-
 h1 {
     text-align: center;
 }
